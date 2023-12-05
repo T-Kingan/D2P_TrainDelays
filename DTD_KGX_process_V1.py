446f51d4dda41f1fd2cec7705c02200bb44704ba
T-Kingan <73171307+T-Kingan@users.noreply.github.com>
2023-12-05 12:36:46 +0000

    Update DTD_KGX_process_V1.py

--- DTD_KGX_process_V1.py
+++ DTD_KGX_process_V1.py
@@ -1,5 +1,9 @@
 import csv
 
+import csv
+from datetime import datetime, timedelta  # Import the datetime module
+
+
 mca_file_path = r'C:\Users\thoma\OneDrive - Imperial College London\Des Eng Y4\Data2Product\Coursework\timetable_data_V2\RJTTF937.MCA'
 
 output_directory = r'C:\Users\thoma\OneDrive - Imperial College London\Des Eng Y4\Data2Product\Coursework\timetable_data_V2'
@@ -8,6 +12,8 @@ extension = ".csv"
 
 bs_file_path = f"{output_directory}/Basic_Schedule_RJTTF937{extension}"
 locations_file_path = f"{output_directory}/Locations_RJTTF9373{extension}"
+
+timetable_csv_path = f"{output_directory}/KingsCross_Edinburgh_Timetable.csv"
 
 # Define TIPLOC codes for the stations
 origin_tiploc = 'KNGX'       # London Kings Cross
@@ -42,88 +48,197 @@ bs_field_mapping = {
     'STP Indicator': (79, 80)
 }
 
+origin_location_field_mapping = {
+    'Record Identity': (0, 2),
+    'Location': (2, 10),
+    'Scheduled Departure Time': (10, 15),
+    'Public Departure Time': (15, 19),
+    'Platform': (19, 22),
+    'Line': (22, 25),
+    'Engineering Allowance': (25, 27),
+    'Pathing Allowance': (27, 29),
+    'Activity': (29, 41),
+    'Performance Allowance': (41, 43),
+    'Spare': (43, 80)
+}
 
-origin_location_field_mapping = {}
-intermediate_location_field_mapping = {}
-terminating_location_field_mapping = {}
+intermediate_location_field_mapping = {
+    'Record Identity': (0, 2),
+    'Location': (2, 10),
+    'Scheduled Arrival Time': (10, 15),
+    'Scheduled Departure Time': (15, 20),
+    'Scheduled Pass': (20, 25),
+    'Public Arrival': (25, 29),
+    'Public Departure': (29, 33),
+    'Platform': (33, 36),
+    'Line': (36, 39),
+    'Path': (39, 42),
+    'Activity': (42, 54),
+    'Engineering Allowance': (54, 56),
+    'Pathing Allowance': (56, 58),
+    'Performance Allowance': (58, 60),
+    'Spare': (60, 80)
+}
 
+terminating_location_field_mapping = {
+    'Record Identity': (0, 2),
+    'Location': (2, 10),
+    'Scheduled Arrival Time': (10, 15),
+    'Public Arrival Time': (15, 19),
+    'Platform': (19, 22),
+    'Path': (22, 25),
+    'Activity': (25, 37),
+    'Spare': (37, 80)
+}
 
-def parse_mca_to_csv(mca_file, locations_file, bs_file, origin_tiploc, destination_tiploc):
-    
-    with open(mca_file, 'r') as file, \
-         open(locations_file, 'w', newline='') as locations_file_var, \
-         open(bs_file, 'w', newline='') as bs_file_var:
-        
-        # Need to implement the following somewhere
-        # csvwriter = csv.writer(csvfile)
-        # # Write the header row
-        # csvwriter.writerow(['Departure Date', 'Departure Time', 'Arrival Time'])
+def parse_line(line, field_mapping):
+    parsed_data = {}
+    for field, (start, end) in field_mapping.items():
+        parsed_data[field] = line[start:end].strip()
+    return parsed_data
 
-        bs_csvwriter = csv.DictWriter(bs_file_var, fieldnames=bs_field_mapping.keys())
-        #location_csvwriter = csv.DictWriter(locations_file_var, fieldnames=location_fieldnames)
-        
-        bs_csvwriter.writeheader()
-        #location_csvwriter.writeheader()
+def parse_all_mca(mca_file):
+    all_trains = []
 
+    with open(mca_file, 'r') as file:
         current_train = {}
-
-        #
         for line in file:
             record_type = line[0:2]
 
             if record_type == 'BS':
-                # Start a new train entry
-                
-                # 
-                bs_record = parse_basic_schedule(line)
-                bs_csvwriter.writerow(bs_record)
+                if current_train:  # If current_train is not empty
+                    all_trains.append(current_train)
+                current_train = {'BS': parse_line(line, bs_field_mapping)}
 
-            # next line is a BX record
-            # followed by LO
-            # followed by LI for each intermediate location
-            # followed by LT
+            elif record_type in ['LO', 'LI', 'LT']:
+                location_mapping = {
+                    'LO': origin_location_field_mapping,
+                    # --- Doesn't have all the fields of the intermediate location ---
+                    # For example if its actually going to aberdeen it might miss edinburgh
+                    'LI': intermediate_location_field_mapping,
+                    'LT': terminating_location_field_mapping
+                }
+                parsed_data = parse_line(line, location_mapping[record_type])
+                current_train[record_type] = parsed_data
+
+                if record_type == 'LT':
+                    all_trains.append(current_train)
+                    current_train = {}  # Reset for the next train entry
+        
+        if current_train:  # Add the last train entry if the file does not end with an 'LT'
+            all_trains.append(current_train)
+
+    return all_trains
+
+def filter_dept_dest(all_trains, origin_tiploc, destination_tiploc):
+    filtered_trains = []
+
+    for train in all_trains:
+        if 'BS' in train and 'LO' in train and 'LT' in train:
+            origin_location = train['LO']
+            destination_location = train['LT']
+
+            if (
+                origin_location['Location'] == origin_tiploc
+                and destination_location['Location'] == destination_tiploc
+            ):
+                # Train originates at Kings Cross and terminates at Edinburgh
+                filtered_trains.append(train)
+            # --- Need to include if Train originates at Kings Cross and has an intermediary stop at Edinburgh e.g. Aberdeen ---
+            # elif origin_location['Location'] == origin_tiploc:
+            #     timetable.append(train)
+
+    return filtered_trains
+
+def construct_timetable(filtered_trains, from_date, to_date):
+  # create a dictionary where each key is a day and the corresponding value is a list of trains that day
+  # construct a list of days between from_date and to_date
+  timetable = {}
+  # Calculate the range of dates between from_date and to_date
+  current_date = from_date
+  while current_date <= to_date:
+      # Format the current_date as a string in the desired format, e.g., 'YYYY-MM-DD'
+      current_date_str = current_date.strftime('%Y-%m-%d')
+      
+      # Initialize an empty list of trains for this day
+      timetable[current_date_str] = []
+      
+      # Move to the next day
+      current_date += timedelta(days=1)
+  # for each train, check if it runs on each day in the list of days
+  # if it does, add it to the list of trains for that day
+  for train in filtered_trains:
+      # Get the days that this train runs
+      days_run = train['BS']['Days Run']
+      
+      # Get the start and end dates for this train
+      date_runs_from = datetime.strptime(train['BS']['Date Runs From'], '%y%m%d')
+      date_runs_to = datetime.strptime(train['BS']['Date Runs To'], '%y%m%d')
+
+      # Check if the train runs on each day in the timetable
+      current_date = from_date
+      while current_date <= to_date:
+          # Format the current_date as a string in the desired format, e.g., 'YYYY-MM-DD'
+          current_date_str = current_date.strftime('%Y-%m-%d')
+          # Check if the train runs on this day
+          if (
+              current_date >= date_runs_from
+              and current_date <= date_runs_to
+              and days_run[current_date.weekday()] == '1'
+          ):
+              # Add the train to the list of trains for this day
+              # Add more specific information about the train
+              dept_time = train['LO']['Public Departure Time'] # format into time
+              arr_time = train['LT']['Public Arrival Time'] # format into time
+
+              timetable[current_date_str].append({
+                  'Train UID': train['BS']['Train UID'],
+                  'Departure Time': dept_time,
+                  'Arrival Time': arr_time
+              })
+          
+          # Move to the next day
+          current_date += timedelta(days=1)
+  return timetable
+
+# Function to write the timetable to a CSV file
+def write_timetable_to_csv(timetable, output_path):
+    with open(output_path, 'w', newline='') as csvfile:
+        fieldnames = ['Date', 'Train UID', 'Departure Time', 'Arrival Time']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        # Iterate over each day in the timetable
+        for date, trains in timetable.items():
+            # Write each train's information as a row in the CSV file
+            for train in trains:
+                writer.writerow({
+                    'Date': date,
+                    'Train UID': train['Train UID'],
+                    'Departure Time': train['Departure Time'],
+                    'Arrival Time': train['Arrival Time']
+                })
+  
+from_date = datetime.strptime('2023-12-01', '%Y-%m-%d')
+to_date = datetime.strptime('2024-01-01', '%Y-%m-%d')
+
+all_trains = parse_all_mca(mca_file_path)
+filtered_trains = filter_dept_dest(all_trains, origin_tiploc, destination_tiploc)
+new_timetable = construct_timetable(filtered_trains, from_date, to_date)
+write_timetable_to_csv(new_timetable, timetable_csv_path)
 
 
-            # --- To be implemented ---
-            # elif record_type in ['LO', 'LI', 'LT']: # PERHAPS separate by location type? They are formatted differently
-            #     location_record = parse_location_record(line)
-                
-            #     if location_record['tiploc'] == origin_tiploc and record_type == 'LO':
-            #         current_train['departure_date'] = current_train['date_runs_from']
-            #         current_train['departure_time'] = location_record['scheduled_departure']
 
-            #     elif location_record['tiploc'] == destination_tiploc and record_type == 'LT':
-            #         current_train['arrival_time'] = location_record['scheduled_arrival']
-            #         csvwriter.writerow([current_train.get('departure_date'), 
-            #                             current_train.get('departure_time'), 
-            #                             current_train.get('arrival_time')])
+    # # Write to Basic Schedule CSV file
+    # with open(bs_file, 'w', newline='') as bs_file_var:
+    #     bs_csvwriter = csv.DictWriter(bs_file_var, fieldnames=bs_field_mapping.keys())
+    #     bs_csvwriter.writeheader()
+    #     for train in all_trains:
+    #         if 'BS' in train:
+    #             bs_csvwriter.writerow(train['BS'])
 
-def parse_basic_schedule(line):
-    # Extract fields from the line based on fixed-width values
-    bs_parsed_data = {}
-    for field, (start, end) in bs_field_mapping.items():
-        # Adjust the start index for zero-based indexing and set the end index for slicing
-        bs_parsed_data[field] = line[start:end].strip()
-    
-    return bs_parsed_data
+    # Process all_trains to create the timetable
+    # This step will need to filter the trains for those starting at Kings Cross and ending at Edinburgh
+    # and then extract and write the necessary timetable information to another CSV file
 
-def parse_location_record(line):  # Not all location records are the same format...
-    # Extract fields from the line based on fixed-width values
-    return {
-        'tiploc': line[2:9].strip(), # Position 3-10?
-        'scheduled_arrival': line[10:15].strip(),
-        'scheduled_departure': line[15:20].strip(),
-        # ... add other relevant fields
-    }
-
-def parse_origin_location(line):
-    return {
-        'tiploc': line[2:9].strip(),
-        'scheduled_departure': line[10:15].strip(),
-    }
-
-
-
-
-# Parse the .MCA file and write to CSV
-parse_mca_to_csv(mca_file_path, locations_file_path, bs_file_path, origin_tiploc, destination_tiploc)
+# Call the function with the correct file paths
