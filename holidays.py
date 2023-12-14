4bd25705e35b48b969015789da3627968db541bb
BertEdwards <73135489+BertEdwards@users.noreply.github.com>
2023-12-14 02:15:35 +0000

    Add files via upload

--- holidays.py
+++ holidays.py
@@ -5,6 +5,7 @@
 import pandas as pd
 bank_holidays = pd.read_json(path_or_buf = 'https://www.gov.uk/bank-holidays.json')
 
+#Calling from the link
 def get_england_and_wales(data_frame):
     return (
         pd
@@ -22,23 +23,26 @@ def get_england_and_wales(data_frame):
 
 bh = get_england_and_wales(bank_holidays)
 
-start_date = '2019-12-31'
-end_date = '2023-3-31'
+# Setting date range
+start_date = '2021-01-01'
+start_date_less_one = '2020-12-31'
+end_date = '2023-12-02'
 
-bh_by_date = bh.query('date > @start_date and date <= @end_date')
+# new data frame with the set date interval
+bh_by_date = bh.query('date > @start_date_less_one and date <= @end_date')
 #print(bh_by_date)
 
 output_file_path = '/Users/bert/Documents/Documents - Bert’s MacBook Pro/Imperial/Data to Product/CW/LDN - EDI/holidays.csv' 
 
-# Save the sorted DataFrame to a new CSV file
-bh_by_date.to_csv(output_file_path, index=False)
+# Save the DataFrame to a new CSV file
+#bh_by_date.to_csv(output_file_path, index=False)
 
 #/////////////////////////////
 # This part of the code creates a data frame with all dates 1/1/20-31/3/23 then adds in public holiday y/n
 
 
 # Creating a date range from 1/1/2020 to 31/3/2023
-date_range = pd.date_range(start="2020-01-01", end="2023-03-31")
+date_range = pd.date_range(start=start_date, end=end_date)
 
 # Converting the public holiday dates to datetime for comparison
 bh_by_date['date'] = pd.to_datetime(bh_by_date['date'])
@@ -55,4 +59,7 @@ date_df['two_days_before_holiday'] = date_df['is_holiday'].shift(-2).fillna(0).a
 date_df['day_after_holiday'] = date_df['is_holiday'].shift(1).fillna(0).astype(int)
 date_df['two_days_after_holiday'] = date_df['is_holiday'].shift(2).fillna(0).astype(int)
 
-print(date_df.head() ) # Display the first few rows of the new DataFrame
+#print(date_df.tail() ) # Display the first few rows of the new DataFrame
+
+output_file_path2 = '/Users/bert/Documents/Documents - Bert’s MacBook Pro/Imperial/Data to Product/CW/LDN - EDI/Final Data Sources/01-01-2021_to_02-12-23_holidays.csv' 
+date_df.to_csv(output_file_path2, index=False)
